ef3f49b8eddf0f0f3ba332fdccb869ccf27bba53
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2021-12-04 23:43:45 +0000

    Generating a 32 bit nonce

--- src/flickr.ts
+++ src/flickr.ts
@@ -2,9 +2,32 @@ import jsSHA from "jssha";
 
 export async function flickrAuthGetRequestToken(request: Request): Promise<Response> {
     const oauth_timestamp:number = Math.round((new Date()).getTime() / 1000.0);
+
+    let integerArray:Uint32Array = new Uint32Array(1)
+    crypto.getRandomValues( integerArray )
+
+    let oauthNonce:number = integerArray[0];
+
+    const baseStringComponents:string[] = [
+        FGA_FLICKR_OAUTH_REQUEST_TOKEN_VERB,
+        FGA_FLICKR_OAUTH_REQUEST_TOKEN_ENDPOINT_URI,
+        "oauth_callback=" + FGA_FLICKR_OAUTH_CALLBACK_URI,
+        "oauth_consumer_key=" + FGA_FLICKR_OAUTH_APP_KEY,
+        "oauth_nonce=" + oauthNonce,
+        "oauth_signature_method=" + "",
+        "oauth_timestamp=" + "",
+        "oauth_version=" + "",
+    ];
+
+    let encodedBaseString:string = "";
+
+    for ( let currComponent of baseStringComponents ) {
+        encodedBaseString += encodeURIComponent( currComponent ) + "&"
+    }
+
+    // TODO: trim off last ampsersand
     
 
-    return new Response('Getting Flicker request token\n\nOAuth timestamp: ' + oauth_timestamp +
-        "\n\nOauth callback: " + FGA_FLICKR_OAUTH_CALLBACK_URI )
+    return new Response("Getting Flicker request token\n\nEncoded base string: " + encodedBaseString )
  
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,8 @@
 export {};
 
 declare global {
+    const FGA_FLICKR_OAUTH_REQUEST_TOKEN_VERB: string;
+    const FGA_FLICKR_OAUTH_REQUEST_TOKEN_ENDPOINT_URI: string;
     const FGA_FLICKR_OAUTH_CALLBACK_URI: string;
+    const FGA_FLICKR_OAUTH_APP_KEY: string;
 }
